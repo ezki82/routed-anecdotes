58973ec573b1fefb4d6cbdd377187d7f2982a206
Esa Salminen <esa_salminen@hotmail.com>
2021-11-01 20:42:07 +0200

    7.3 tehty, 0,5h

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import {
   BrowserRouter as Router,
-  Switch, Route, Link, useParams
+  Switch, Route, Link, useParams, useHistory
 } from 'react-router-dom'
 
 const Menu = () => {
@@ -53,6 +53,8 @@ const CreateNew = (props) => {
   const [author, setAuthor] = useState('')
   const [info, setInfo] = useState('')
 
+  const history = useHistory()
+
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -62,6 +64,7 @@ const CreateNew = (props) => {
       info,
       votes: 0
     })
+    history.push('/')
   }
 
   return (
@@ -122,6 +125,10 @@ const App = () => {
   const addNew = (anecdote) => {
     anecdote.id = (Math.random() * 10000).toFixed(0)
     setAnecdotes(anecdotes.concat(anecdote))
+    setNotification(`a new anecdote ${anecdote.content} created!`)
+    setTimeout(() => {
+      setNotification('')
+    }, 10000);
   }
 
   const anecdoteById = (id) =>
@@ -143,7 +150,7 @@ const App = () => {
       <div>
         <h1>Software anecdotes</h1>
         <Menu />
-
+        {notification === '' ? <></> : <div>{notification}</div>}
         <Switch>
           <Route path="/about">
             <About />
