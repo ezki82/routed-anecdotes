76120559ade1ccf814a22d84ababb98a3547c132
Esa Salminen <esa_salminen@hotmail.com>
2021-11-02 21:31:39 +0200

    7.5 ja 7.6 tehty, 2h

--- src/App.js
+++ src/App.js
@@ -74,17 +74,23 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input {...content}/>
+          <input {...content.input}/>
         </div>
         <div>
           author
-          <input {...author} />
+          <input {...author.input} />
         </div>
         <div>
           url for more info
-          <input {...info}/>
+          <input {...info.input}/>
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button onClick={(e) => {
+          e.preventDefault()
+          content.reset()
+          author.reset()
+          info.reset()
+        }}>reset</button>
       </form>
     </div>
   )
